f4bd6c02c26535c2540e13ca90ae591a86d5f9d9
Tuomo Syvänperä <tuomo.syvanpera@gmail.com>
2020-05-15 09:47:01 +0300

    Add arandr layouts

---
+++ .screenlayout/3-monitor.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP1 --primary --mode 3000x2000 --pos 0x1440 --rotate normal --output DP1 --mode 2560x1440 --pos 2780x0 --rotate normal --output DP2 --mode 2560x1440 --pos 220x0 --rotate normal --output HDMI1 --off --output VIRTUAL1 --off
