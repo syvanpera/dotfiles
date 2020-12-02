dadb3ad3ba7c7a69469361554f3dd9aff8a385ca
Tuomo Syvänperä <tuomo.syvanpera@gmail.com>
2020-12-02 09:47:29 +0200

    Fix multimonitor setup for polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-DP1_STATUS=$(</sys/class/drm/card0/card0-DP-1/status )
+# DP1_STATUS=$(</sys/class/drm/card0/card0-DP-1/status )
+#
+# if [ -f /sys/class/drm/card0/card0-DP-2/status ]; then
+#     DP2_STATUS=$(</sys/class/drm/card0/card0-DP-2/status )
+# fi
 
-if [ -f /sys/class/drm/card0/card0-DP-2/status ]; then
-    DP2_STATUS=$(</sys/class/drm/card0/card0-DP-2/status )
-fi
+DISPLAYS=$(xrandr --listactivemonitors| grep 'Monitors:'| awk '{print $2}')
 
 # Terminate already running bar instances
 killall -q polybar
@@ -22,19 +24,22 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 #     polybar --reload main &
 # fi
 
-if [ "connected" == "$DP1_STATUS" ]; then
+# if [ "connected" == "$DP1_STATUS" ]; then
+if [ $DISPLAYS -gt 1 ]; then
     polybar external -c ~/.config/polybar/config &
     ln -s /tmp/polybar_mqueue.$! /tmp/ipc-polybar1
+else
+    polybar main -c ~/.config/polybar/config &
 fi
 # if [ "connected" == "$DP2_STATUS" ]; then
 #     polybar dp2 -c ~/.config/polybar/config &
 #     ln -s /tmp/polybar_mqueue.$! /tmp/ipc-polybar2
 # fi
 
-if [ "connected" == "$DP1_STATUS" ] || [ "connected" == "$DP2_STATUS" ]; then
-    # polybar edp1 -c ~/.config/polybar/config &
-    ln -s /tmp/polybar_mqueue.$! /tmp/ipc-polybar3
-else
-    polybar main -c ~/.config/polybar/config &
-    ln -s /tmp/polybar_mqueue.$! /tmp/ipc-polybar1
-fi
+# if [ "connected" == "$DP1_STATUS" ] || [ "connected" == "$DP2_STATUS" ]; then
+#     # polybar edp1 -c ~/.config/polybar/config &
+#     ln -s /tmp/polybar_mqueue.$! /tmp/ipc-polybar3
+# else
+#     polybar main -c ~/.config/polybar/config &
+#     ln -s /tmp/polybar_mqueue.$! /tmp/ipc-polybar1
+# fi
