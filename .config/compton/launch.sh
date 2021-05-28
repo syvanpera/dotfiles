62629457cc4fe62248db7e251cc700437843e171
Tuomo Syvänperä <tuomo.syvanpera@gmail.com>
2021-05-28 21:53:27 +0300

    Remove extra stuff not needed in Fedora with Wayland

--- .config/compton/launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-killall -q picom
-
-# Wait until the processes have been shut down
-while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
-
-picom -cC --config .config/compton/config &
