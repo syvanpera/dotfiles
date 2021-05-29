d4486becd5231765351310b40370a94e0e40482a
Tuomo Syvänperä <tuomo.syvanpera@gmail.com>
2021-05-29 10:06:19 +0300

    Add tmux conf and some fish funcs

--- .config/fish/functions/l.fish
+++ .config/fish/functions/l.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function l --description 'alias l exa -l --color=always --group-directories-first --time-style=long-iso'
-	exa -l --color=always --group-directories-first --time-style=long-iso $argv;
+function l --description 'alias l exa -l --group --color=always --group-directories-first --time-style=long-iso --icons'
+	exa -l --group --color=always --group-directories-first --time-style=long-iso --icons $argv;
 end
--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function ls --description 'alias ls exa --color=always --group-directories-first --time-style=long-iso'
-	exa --color=always --group-directories-first --time-style=long-iso $argv;
+function ls --description 'alias ls exa --group --color=always --group-directories-first --time-style=long-iso --icons'
+	exa --group --color=always --group-directories-first --time-style=long-iso --icons $argv;
 end
