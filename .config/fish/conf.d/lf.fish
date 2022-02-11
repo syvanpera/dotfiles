4cd0af34d6de2e41e82dce24af1cd1411de7f6c4
Tuomo Syvänperä <tuomo.syvanpera@gmail.com>
2022-02-11 10:02:46 +0200

    Some WSL tweaks

--- .config/fish/conf.d/lf.fish
+++
@@ -1,2 +0,0 @@
-# set -U LF_ICONS (tr '\n' ':' <~/.config/lf/lf-icons)
-export LF_ICONS=(tr '\n' ':' <~/.config/lf/lf-icons)
---
+++ .config/fish/config.fish
@@ -0,0 +1,3 @@
+# Generated for envman. Do not edit.
+test -s "$HOME/.config/envman/load.fish"; and source "$HOME/.config/envman/load.fish"
+
