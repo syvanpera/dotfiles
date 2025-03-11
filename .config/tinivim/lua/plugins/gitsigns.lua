b3ba2c2eb1ec8eca2d846f6b328cd9e465900d0b
Tuomo Syvänperä <tuomo.syvanpera@gmail.com>
2025-03-11 09:29:43 +0200

    Tweak gitsigns

--- .config/tinivim/lua/plugins/gitsigns.lua
+++ .config/tinivim/lua/plugins/gitsigns.lua
@@ -1,4 +1,17 @@
 return {
   "lewis6991/gitsigns.nvim",
-  opts = {},
+  opts = {
+    signs_staged_enable = true,
+    signcolumn = true,
+    numhl = true,
+    current_line_blame = true,
+    current_line_blame_opts = {
+      virt_text = true,
+      virt_text_pos = "right_align", -- or 'eol'
+      delay = 1000,
+      ignore_whitespace = false,
+      virt_text_priority = 100,
+      use_focus = true,
+    },
+  },
 }
